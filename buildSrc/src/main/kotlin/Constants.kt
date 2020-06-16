d45ae9ac428b3cf753d93d36531ea8601963e15a
Eugene Popovich <httpdispatch@gmail.com>
2020-06-16 09:32:12 +0300

    Update compile SDK version and build tools version

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,6 +1,6 @@
 object Constants {
     const val KOTLIN_VERSION = "1.3.72"
     const val TARGET_SDK_VERSION = 28
-    const val COMPILE_SDK_VERSION = 28
-    const val BUILD_TOOLS_VERSION = "28.0.3"
+    const val COMPILE_SDK_VERSION = 29
+    const val BUILD_TOOLS_VERSION = "29.0.2"
 }
