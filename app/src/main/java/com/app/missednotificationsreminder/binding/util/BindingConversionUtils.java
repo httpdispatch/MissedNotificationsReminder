e1647c60eb59a633db0b68bd08eff6f0d9490f42
Eugene Popovich <httpdispatch@gmail.com>
2020-08-04 09:44:48 +0300

    Remove unused databinding related functionality

--- app/src/main/java/com/app/missednotificationsreminder/binding/util/BindingConversionUtils.java
+++ app/src/main/java/com/app/missednotificationsreminder/binding/util/BindingConversionUtils.java
@@ -9,39 +9,6 @@ import android.view.View;
  * @author Eugene Popovich
  */
 public class BindingConversionUtils {
-    /**
-     * Rule to convert {@link BindableString} object to {@link String}
-     *
-     * @param observable the observable string to convert
-     * @return the String value wrapped by the observable parameter
-     */
-    @BindingConversion public static String convertBindableToString(
-            BindableString observable) {
-        return observable.get();
-    }
-
-    /**
-     * Rule to convert {@link BindableBoolean} object to {@link Boolean}
-     *
-     * @param observable the observable boolean to convert
-     * @return the Boolean value wrapped by the observable parameter
-     */
-    @BindingConversion public static Boolean convertBindableToBoolean(
-            BindableBoolean observable) {
-        return observable == null ? false : observable.get();
-    }
-
-
-    /**
-     * Rule to convert {@link BindableBoolean} object to int visibility code:
-     * either {@link View#VISIBLE} or {@link View#GONE}
-     *
-     * @param observable the observable boolean to convert
-     * @return the the visibility code depend on visible value wrapped by the observable parameter
-     */
-    @BindingConversion public static int convertBindingToVisibility(BindableBoolean observable) {
-        return convertBooleanToVisibilityState(observable == null ? null : observable.get());
-    }
 
     /**
      * Rule to convert {@link Boolean} object to int visibility code:
