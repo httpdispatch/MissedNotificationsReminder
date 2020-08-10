87afca783e137daa4a7a9ffd523e4e6084f97319
Eugene Popovich <httpdispatch@gmail.com>
2020-08-10 07:48:20 +0300

    Use mutable state flow instead of ConflatedBroadcastChannel in the FlowEventBus

--- app/src/main/java/com/app/missednotificationsreminder/util/event/FlowEventBus.kt
+++ app/src/main/java/com/app/missednotificationsreminder/util/event/FlowEventBus.kt
@@ -2,9 +2,8 @@ package com.app.missednotificationsreminder.util.event
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.channels.ConflatedBroadcastChannel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.drop
 import timber.log.Timber
 
@@ -16,17 +15,17 @@ import timber.log.Timber
 class FlowEventBus {
     // If multiple threads are going to emit events to this
     // then it must be made thread-safe like this instead
-    private val _bus = ConflatedBroadcastChannel<Event>(object : Event {})
+    private val _bus = MutableStateFlow<Event>(object : Event {})
 
     /**
      * Send the event to all the subscribers
      *
      * @param event the event to send
      */
-    suspend fun send(event: Event) {
+    fun send(event: Event) {
         Timber.d("send() called with: event = %s",
                 event)
-        _bus.send(event)
+        _bus.value = event
     }
 
     /**
@@ -34,5 +33,5 @@ class FlowEventBus {
      *
      * @return
      */
-    fun toFlow(): Flow<Event> = _bus.asFlow().drop(1)
+    fun toFlow(): Flow<Event> = _bus.drop(1)
 }
