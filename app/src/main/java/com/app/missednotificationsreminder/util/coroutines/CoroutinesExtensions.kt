cf27ea1f37723a8932ced5552f15ff86bb06b6bd
Eugene Popovich <httpdispatch@gmail.com>
2020-09-14 10:36:24 +0300

    Add retryCallOnError functionality implementation for suspend blocks

--- app/src/main/java/com/app/missednotificationsreminder/util/coroutines/CoroutinesExtensions.kt
+++ app/src/main/java/com/app/missednotificationsreminder/util/coroutines/CoroutinesExtensions.kt
@@ -1,9 +1,12 @@
 package com.app.missednotificationsreminder.util.coroutines
 
+import com.app.missednotificationsreminder.data.ResultWrapper
+import com.app.missednotificationsreminder.data.asResultWrapper
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
 fun <T> debounce(
         waitMs: Long,
@@ -18,4 +21,29 @@ fun <T> debounce(
             destinationFunction(param)
         }
     }
+}
+
+/**
+ * Retry call in case of error until [maxRetryCount] is reached. In addition log some verbose information
+ */
+suspend fun <T> retryCallOnError(maxRetryCount: Int, block: suspend () -> T): ResultWrapper<T> {
+    require(maxRetryCount >= 0)
+    var result: Result<T>? = null
+    for (count in 0..maxRetryCount) {
+        if (count > 0) {
+            Timber.d("retryCallOnError: retry")
+        }
+        result = runCatching { block() }
+        if (result.isFailure) {
+            Timber.e(result.exceptionOrNull(), "retryCallOnError")
+            Timber.d("retryCallOnError: count=%d; maxRetryCount=%d", count, maxRetryCount)
+        } else {
+            break
+        }
+    }
+    require(result != null)
+    if (result.isFailure) {
+        Timber.d("retryCallOnError: don't retry, max retry count reached")
+    }
+    return result.asResultWrapper()
 }
